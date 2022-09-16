f584dc123ab16307d1adee42f327c01d446424e3
Ihor Zheludkov <500griven@gmail.com>
2022-09-16 12:12:54 +0300

    added key to list

--- src/components/contactsList.js
+++ src/components/contactsList.js
@@ -4,13 +4,15 @@ export default function ContactsList({ contacts, removeContact }) {
   return (
     <>
       {contacts.map((e, i) => (
-        <Box w={300} m='20px auto'>
+        <Box key={i} w={300} m='20px auto'>
           <Avatar name={e.f} />
           <Heading as='h4' size='lg'>
             {e.f} {e.l}
           </Heading>
           <Text fontSize='xl'>{e.p}</Text>
-          <Button value={i} onClick={removeContact}>Remove</Button>
+          <Button value={i} onClick={removeContact}>
+            Remove
+          </Button>
         </Box>
       ))}
     </>
